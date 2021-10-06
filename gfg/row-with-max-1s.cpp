446be05594c5d4795a39191316e998003e7e0238
Ankush Yadav <ankushyadav9302@gmail.com>
2021-10-06 18:39:43 +0530

    Optimize (memory-wise): row-with-max-1s.cpp

--- gfg/row-with-max-1s.cpp
+++ gfg/row-with-max-1s.cpp
@@ -3,22 +3,20 @@
 class Solution{
 public:
 	int rowWithMax1s(vector<vector<int>> arr, int n, int m) {
-	    int maxCount = 0, tmpCount, index = -1;
+	    int maxCount = 0, index = -1;
 
 	    for (int i = 0; i < n; i++) {
-	        tmpCount = 0;
 	        for (int j = 0; j < m; j++) {
 	            if (arr[i][j] == 1) {
-	                tmpCount = m - j;
+	                if (m - j > maxCount) {
+	                    index = i;
+	                    maxCount = m - j;
+	                }
 	                break;
 	            }
 	        }
 
-	        if (tmpCount > maxCount) {
-	            index = i;
-	            maxCount = tmpCount;
-                if (maxCount == m) { break; }
-	        }
+	        if (maxCount == m) { break; }
 	    }
 
 	    return index;
