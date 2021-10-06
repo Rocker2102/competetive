4b8612a9cb06fc52fb06a5b59a87f22def13c81c
Chander Jindal <72246687+ChanderJindal@users.noreply.github.com>
2021-10-06 14:53:07 +0530

    Add Maximize It! (#1)
    Author  -> anuj_95
    Difficulty  -> Hard
    Max Score -> 50

---
+++ hackerrank/python/maximize-it.py
@@ -0,0 +1,27 @@
+def max_value(lst, mod):
+    answer = 0
+    for i in lst:
+        curr = 0
+        for val in i:
+            curr = curr + (val * val)
+        curr = curr % mod
+        if curr > answer:
+            answer = curr
+    print(answer)
+
+num_lsts, mod = map(int, input().split())
+lst_size = [] 
+lst_lst = []
+for i in range(num_lsts):
+    string = input()
+    lst = string.split()
+    lst_size.append(int(lst[0]))
+    lst.remove(lst[0])
+    for i in range(len(lst)):
+        lst[i] = int(lst[i]) % mod
+    lst_lst.append(lst)
+#print(lst_lst)
+import itertools
+lst_lst = list(itertools.product(*lst_lst))
+#print(lst_lst)
+max_value(lst_lst, mod)
