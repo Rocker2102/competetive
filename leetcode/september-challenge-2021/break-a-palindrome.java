669adfe3ea7c32eefd1d1760f1b563896ff2bc97
Ankush Yadav <ankushyadav9302@gmail.com>
2021-10-25 12:52:34 +0530

    Add break-a-palindrome.java

---
+++ leetcode/september-challenge-2021/break-a-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        int len = palindrome.length();
+
+        if (len <= 1) { return ""; }
+
+        short changed = 0;
+        for (int i = 0; i < len / 2; i++) {
+            if (palindrome.charAt(i) != 'a') {
+                palindrome = palindrome.substring(0, i) + 'a' + palindrome.substring(i + 1);
+                changed = 1;
+                break;
+            }
+        }
+
+        if (changed == 0) {
+            palindrome = palindrome.charAt(len - 1) == 'a'
+                ? palindrome.substring(0, len - 1) + 'b'
+                : palindrome.substring(0, len - 1) + 'a';
+        }
+
+        return palindrome;
+    }
+}
