b561cea08290eceec4fdebbc3839625552fb95df
Ankush Yadav <ankushyadav9302@gmail.com>
2021-11-22 03:49:37 +0530

    Add binary-search.cpp

---
+++ leetcode/binary-search.cpp
@@ -0,0 +1,27 @@
+/* https://leetcode.com/problems/binary-search/ */
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int end = nums.size() - 1;
+        int beg = 0;
+        int mid;
+
+        if (end == 0) {
+            if (nums[0] == target) { return 0; }
+            else { return -1; }
+        }
+
+        while (beg <= end) {
+            mid = beg + (end - beg) / 2;
+            if (nums[mid] == target) { return mid; }
+            else if (nums[mid] < target) {
+                beg = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+};
