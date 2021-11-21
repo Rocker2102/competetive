1a85eb0f4c1f6db31177e800f7608be38988733b
Ankush Yadav <ankushyadav9302@gmail.com>
2021-11-21 08:04:01 +0530

    Add day0-hello-world.cpp

---
+++ hackerrank/30-days-of-code/README.md
@@ -0,0 +1,3 @@
+# 30 Days of Code
+
+[:heavy_check_mark:](day0-hello-world.cpp) [Day 0 - Hello World](https://www.hackerrank.com/challenges/30-hello-world/problem)
---
+++ hackerrank/30-days-of-code/day0-hello-world.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+   string inputString; // declare a variable to hold our input
+   getline(cin, inputString); // get a line of input from cin and save it to our variable
+
+   cout << "Hello, World." << endl;
+   cout << inputString << endl;
+
+   return 0;
+}
