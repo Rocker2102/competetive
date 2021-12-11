63a7e25631939183d4623da555c1565bce836b26
Ankush Yadav <ankushyadav9302@gmail.com>
2021-12-11 19:53:04 +0530

    Optimize day25-running-time-and-complexity.java

--- hackerrank/30-days-of-code/day25-running-time-and-complexity.java
+++ hackerrank/30-days-of-code/day25-running-time-and-complexity.java
@@ -5,20 +5,22 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int T = in.nextInt(), n, count = 0;
+        int T = in.nextInt(), n, sqrt, count = 0;
         boolean flag;
 
         while (T > 0) {
             flag = false;
             n = in.nextInt();
+            sqrt = (int)Math.sqrt(n);
             T--;
+
 
             if (n == 1) {
                 System.out.println("Not prime");
             } else if (n == 2) {
                 System.out.println("Prime");
             } else {
-                for (int i = 2; i <= n / 2; i++) {
+                for (int i = 2; i <= sqrt; i++) {
                     if (n % i == 0) {
                         System.out.println("Not prime");
                         flag = true;
